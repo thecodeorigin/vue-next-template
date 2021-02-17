9c8b22fa25cb9c1352f5d8a711654ce7cfa6a8ea
Bittermeatball <nguyenhuunguyeny.ny@gmail.com>
2021-02-17 14:47:17 +0700

    :lock: Add auth routes config

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import about from '@pages/about/config'
 import home from '@pages/home/config'
+import authForgotPassword from '@pages/auth/forgot-password/config'
+import authSignup from '@pages/auth/signup/config'
+import authSignin from '@pages/auth/signin/config'
 import { createRouter, createWebHistory } from 'vue-router'
 import { auth } from '@middleware/auth'
 
@@ -7,7 +10,14 @@ import { auth } from '@middleware/auth'
  * @typedef {import('vue-router').RouteRecordRaw} RouteRecordRaw
  * @type {Array<RouteRecordRaw>}
  */
-const routes = [...about.routes, ...home.routes]
+const routes = [
+  ...about.routes,
+  ...home.routes,
+  // Auth pages
+  ...authForgotPassword.routes,
+  ...authSignup.routes,
+  ...authSignin.routes,
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
