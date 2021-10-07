656acfd108f2814a7c11072d4ce9a41c251473f0
bittermeatball <nguyenhuunguyeny.ny@gmail.com>
2021-10-07 21:52:57 +0700

    feat: add stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-prettier',
+  ],
+  plugins: [
+    'stylelint-scss',
+  ],
+  // add your custom config here
+  // https://stylelint.io/user-guide/configuration
+  rules: {
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['global', 'local', 'root']
+      }
+    ],
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+    'no-descending-specificity': null,
+  },
+};
