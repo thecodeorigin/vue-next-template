3c5739cc3188346b41f9e36bbcabc3c9441bacba
bittermeatball <nguyenhuunguyeny.ny@gmail.com>
2021-10-07 21:47:13 +0700

    feat: seo

--- vite.config.ts
+++ vite.config.ts
@@ -16,23 +16,23 @@ export default defineConfig({
         "apple-touch-icon.png",
       ],
       manifest: {
-        name: "Name of your app",
+        name: "Vue next template",
         short_name: "Short name of your app",
         description: "Description of your app",
         theme_color: "#ffffff",
         icons: [
           {
-            src: "pwa-192x192.png",
+            src: "/img/icons/pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
+            src: "/img/icons/pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
+            src: "/img/icons/pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
